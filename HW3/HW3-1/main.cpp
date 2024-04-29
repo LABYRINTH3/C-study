70108824f0f70f99d7e3aa5da73856e9390510bf
LABYRINTH3 <156741923+LABYRINTH3@users.noreply.github.com>
2024-04-29 19:47:13 +0900

    Update HW3-1

--- HW3/HW3-1/main.cpp
+++ HW3/HW3-1/main.cpp
@@ -27,29 +27,51 @@ void add(int arr3[], int arr1[], int arr2[], int num){
 }
 
 
-
 int main() {
-    int a1 = 4;
-    int b1 = 5;
+    int a1;
+    int b1;
+    // 두 정수를 입력 받는 코드입니다.
+    cout << "첫번째 정수를 입력하시오: ";
+    cin >> a1;
+    cout << "두번째 정수를 입력하시오: ";
+    cin >> b1;
     cout << add(a1, b1) << endl;
 
-    float a2 = 4.5;
-    float b2 = 3.2;
+    float a2;
+    float b2;
+    // 두 실수를 입력 받는 코드입니다.
+    cout << "첫번째 실수를 입력하시오: ";
+    cin >> a2;
+    cout << "두번째 실수를 입력하시오: ";
+    cin >> b2;
     cout << add(a2, b2) << endl;
 
-    int arr_1[4] = {45, 32, 11, 65};
-    cout << add(arr_1, 4) << endl;
+    // 배열의 수를 입력 받는 코드입니다.
+    int numArr;
+    cout << "첫번째 배열의 수를 입력하시오: ";
+    cin >> numArr;
+    // 첫번째 배열을 입력 받는 코드입니다.
+    int arr_1[numArr];
+    for (int i = 0; i < numArr; ++i) {
+        cout << "첫번째 배열의 "<< i <<"번째 정수를 입력하시오: ";
+        cin >> arr_1[i];
+    }
+    cout << add(arr_1, numArr) << endl;
 
-    int arr_2[4] = {11, 43, 32, 56};
+    // 두번째 배열을 입력 받는 코드입니다.
+    cout << "두번째 배열의 수는 " << numArr << "입니다." << endl;
+    int arr_2[numArr];
+
+    for (int i = 0; i < numArr; ++i) {
+        cout << "두번째 배열의 "<< i <<"번째 정수를 입력하시오: ";
+        cin >> arr_2[i];
+    }
     int arr_3[4];
-    // 해당 코드를 작성함으로써 함수에는 주소값을 주었다
-    // 따라서 함수에서 배열을 조작하면 배열 안에 있는 값이 변경된다.
-    add(arr_3, arr_1, arr_2, 4);
+    add(arr_3, arr_1, arr_2, numArr);
 
-    for(int i = 0; i < 4; i++){
+    // 배열의 합의 결과를 출력하는 코드입니다.
+    for (int i = 0; i < 4; i++) {
         cout << arr_3[i] << " ";
     }
-    cout << endl;
-
     return 0;
 }
